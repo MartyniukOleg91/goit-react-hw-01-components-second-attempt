7f475614c337f24ec6d1c20021e7326e1de94516
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-04-03 17:28:40 +0300

    npm

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -29,6 +29,9 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
   );
 };
 
+
+
+
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
