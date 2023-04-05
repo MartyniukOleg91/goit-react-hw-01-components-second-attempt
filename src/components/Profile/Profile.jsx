b033dd6bb0ac8db2ada1d2c6ff6de3f023ff9db5
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-04-06 01:25:08 +0300

    svg

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -10,7 +10,7 @@ export const Profile = ({ username, tag, location, avatar, stats }) => {
       <div className={css.description}>
         <img src={avatar} alt="User avatar" className={css.avatar} />
         <p className={css.name}>{username}</p>
-        <p className={css.tag}><FaApple/>{tag}</p>
+        <p className={css.tag}><FaApple size={20} color="silver" />{tag}</p>
         <p className={css.location}>{location}</p>
       </div>
 
