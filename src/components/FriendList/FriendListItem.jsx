77524786c01f1476fcf6a23341ec38421bfa5190
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-04-06 12:50:52 +0300

    fix

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,15 +1,13 @@
 import css from './FriendList.module.css';
 
-const FriendListItem = ({ dataFriends }) => {
+export const FriendListItem = ({ dataFriends }) => {
   return (
     <div>
       {dataFriends.map(friend => (
         <li className={css.status} key={friend.id}>
-          {friend.isOnline ? (
-            <span className={css.statusOnline}></span>
-          ) : (
-            <span className={css.statusOffline}></span>
-          )}
+          <span
+            className={friend.isOnline ? css.statusOnline : css.statusOffline}
+          />
 
           <img src={friend.avatar} alt="User avatar" width="48" />
           <p>{friend.name}</p>
